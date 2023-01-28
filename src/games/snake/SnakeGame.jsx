0817672ee28a480228b1594ba242745adee285ce
boxak <boxak@naver.com>
2023-01-28 13:33:15 +0900

    2023-01-28

--- src/games/snake/SnakeGame.jsx
+++ src/games/snake/SnakeGame.jsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 const shortId = require('shortid');
 
+class Snake {
+  
+  array = [];
+
+  constructor(row, col) {
+    this.array[0] = {"row" : row, "col" : col};
+  }
+}
+
 const SnakeGame = () => {
 
   const rowCount = 10;
   const colCount = 10;
 
-  const [board, setBoard] = useState([]);
+  const [board, setBoard] = useState({});
+  
+  let snake = undefined;
 
   useEffect(() => {
     let list = [];
@@ -18,19 +29,44 @@ const SnakeGame = () => {
       }
       list.push(temp);
     }
-    setBoard(list);
+    setBoard({"board" : list});
   }, []);
+
+  const startGame = () => {
+    createSnake();
+  }
+
+  const createSnake = () => {
+    const rowPosition = Math.floor(Math.random() * (rowCount - 1)) + 1;
+    const colPosition = Math.floor(Math.random() * (colCount - 1)) + 1;
+
+    let tempArray = [];
+
+    for (let i = 0;i<rowPosition;i++) {
+      let tempRow = [];
+      for (let j = 0;j<colPosition;j++) {
+        if (i === rowPosition && j === colPosition) {
+          tempRow.push(true);
+        } else tempRow.push(false);
+      }
+      tempArray.push(tempRow);
+    }
+
+    setBoard({"board" : tempArray});
+
+    console.log(JSON.stringify(board));
+  }
 
   return (
     <div id="snake-game-div">
       <div id="snake-game-display">
         {
-          board ? board.map((row, key1) => (
+          board.board ? board.board.map((row, key1) => (
             // key = {key1} : Do not use array index as key
             <div className='snake-game-row-div' key={shortId.generate()}>
               {
                 row.map((cell, key2) => (
-                  <div className='snake-game-cell-div' key={shortId.generate()}>
+                  <div className={'snake-game-cell-div' + (cell ? ' snake' : '')} key={shortId.generate()}>
                   </div>
                 ))
               }
@@ -38,7 +74,10 @@ const SnakeGame = () => {
           )) : null
         }
       </div>
-      
+      <div id="snake-game-btn-div">
+        <button id="snake-game-start-btn"
+          onClick={startGame}>시작하기</button>
+      </div>
     </div>
   );
 };
